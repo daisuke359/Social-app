0dfc02a9998ec10a986fc4e8e5d51397059ac66a
Daisuke <dk.joker.1107@icloud.com[credential]>
2021-05-22 14:31:41 -0700

    add axios to get data from database

--- client/src/components/feed/Feed.jsx
+++ client/src/components/feed/Feed.jsx
@@ -1,16 +1,29 @@
-import React from 'react';
+import {useEffect, useState} from 'react';
 import Post from '../post/Post';
 import Share from '../share/Share';
 import "./feed.css";
-import {Posts} from "../../dummyData";
+import axios from "axios";
 
 export default function Feed() {
+
+    const [posts,setPosts] = useState([]);
+
+    useEffect(() => {
+        const fetchPosts = async () => {
+            const res = await axios.get("posts/timeline/609c77bf7055d65afc93390f");
+            setPosts(res.data);
+        }
+
+        fetchPosts();
+        
+    }, []);
+
     return (
         <div className="feed">
            <div className="feedWrapper">
                <Share/>
-               {Posts.map(p => (
-                    <Post key={p.id} post={p}/>
+               {posts.map(p => (
+                    <Post key={p._id} post={p}/>
                ))}
                
            </div>
