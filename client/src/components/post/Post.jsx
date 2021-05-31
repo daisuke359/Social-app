a0b6ac96fe433a152d4a8f97bd313763422a9753
Daisuke <dk.joker.1107@icloud.com[credential]>
2021-05-30 21:42:18 -0700

    Add register and like/dislike function

--- client/src/components/post/Post.jsx
+++ client/src/components/post/Post.jsx
@@ -1,9 +1,10 @@
-import {useState, useEffect} from "react";
+import {useState, useEffect, useContext} from "react";
 import "./post.css";
 import {MoreVert} from "@material-ui/icons";
 import axios from "axios";
 import {format} from "timeago.js";
 import {Link} from "react-router-dom";
+import {AuthContext} from "../../context/AuthContext";
 
 export default function Post({post}) {
 
@@ -11,6 +12,11 @@ export default function Post({post}) {
     const [isLiked, setIsLiked] = useState(false);
     const [user, setUser] = useState({});
     const PF = process.env.REACT_APP_PUBLIC_FOLDER;
+    const {user:currentUser} = useContext(AuthContext);
+
+    useEffect(() => {
+        setIsLiked(post.likes.includes(currentUser._id));
+    }, [currentUser._id, post.likes]);
 
     useEffect(() => {
         const fetchUser = async () => {
@@ -23,6 +29,12 @@ export default function Post({post}) {
     }, [post.userId]);
 
     const likeHandler = () => {
+        try {
+            axios.put("posts/" + post._id + "/like", {userId: currentUser._id});
+        }
+        catch(err) {
+            console.log(err);
+        }
         setLike(isLiked ? like - 1 : like + 1);
         setIsLiked(!isLiked);
     }
@@ -33,7 +45,7 @@ export default function Post({post}) {
                 <div className="postTop">
                     <div className="postTopLeft">
                         <Link to={`profile/${user.username}`}>
-                            <img className="postProfileImg" src={user.profilePicture || PF + "person/noAvatar.png"} alt="" />
+                            <img className="postProfileImg" src={user.profilePicture ? PF+user.profilePicture : PF + "person/noAvatar.png"} alt="" />
                         </Link>
                         <span className="postUsername">{user.username}</span>
                         <span className="postDate">{format(post.createdAt)}</span>
--- client/src/components/topbar/Topbar.jsx
+++ client/src/components/topbar/Topbar.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import "../topbar/topbar.css";
 import { Search, Person, Chat, Notifications } from "@material-ui/icons";
 import {Link} from "react-router-dom";
+import {AuthContext} from "../../context/AuthContext";
 
 export default function Topbar() {
+
+    const {user} = useContext(AuthContext);
+    const PF = process.env.REACT_APP_PUBLIC_FOLDER;
+
     return (
         <div className="topbarContainer">
             <div className="topbarLeft">
@@ -34,7 +39,10 @@ export default function Topbar() {
                         <span className="topbarIconBadge">1</span>
                     </div>
                 </div>
-                <img src="/assets/person/1.jpeg" alt="" className="topbarImg" />
+                <Link to={`/profile/${user.username}`}>
+                    <img src={user.profilePicture ? PF+user.profilePicture : PF+"person/noAvatar.png"} alt="" className="topbarImg" />
+                </Link>
+                
 
             </div>
 
