481ae4ca01966f56071c636ccce739ad3b020309
Daisuke <dk.joker.1107@icloud.com[credential]>
2021-07-24 14:39:43 -0700

    add search function

--- client/src/components/online/Online.jsx
+++ client/src/components/online/Online.jsx
@@ -6,16 +6,6 @@ export default function Online({user}) {
 
     const PF = process.env.REACT_APP_PUBLIC_FOLDER;
 
-    // return (
-    //     <li className="rightbarFriend">
-    //         <Link to={`/profile/${user.username}`} className="rightbarProfileImgContainer">
-    //             <img src={user.profilePicture ? PF + user.profilePicture : PF+"person/noAvatar.png"} alt="" className="rightbarProfileImg" />
-    //             <span className="rightbarOnline"></span>
-    //         </Link>
-    //         <span className="rightbarUsername">{user.username}</span>
-    //     </li>
-    // )
-
     return (
         <Link to={`/profile/${user.username}`} className="rightbarProfileImgContainer">
         <li className="rightbarFriend">
